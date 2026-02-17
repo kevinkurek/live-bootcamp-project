0c6c7bf3cc05f9819f2651ba183ec19c4fdc888a
kevinkurek <kevinkurek@live.com>
2026-02-16 17:28:54 -0800

    [Sprint 1][Task 3] restructue auth service for testing

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+
+use axum::Router;
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: axum::serve::Serve<TcpListener, Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // Move the Router definition from `main.rs` to here.
+        let assets_dir = ServeDir::new("assets");
+        let router = Router::new().fallback_service(assets_dir);
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok( Self {server, address})
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
