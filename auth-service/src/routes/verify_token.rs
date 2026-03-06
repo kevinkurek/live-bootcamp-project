8f5737a6960f57912423057004890c894442851b
kevinkurek <kevinkurek@live.com>
2026-03-06 15:09:25 -0800

    [Sprint 3][Task 4] verify_token routes & tests, local pass and login 200

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,20 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{Json, http::StatusCode, response::IntoResponse};
+use serde::Deserialize;
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{domain::AuthAPIError, utils::auth::validate_token};
+
+pub async fn verify_token(
+    Json(request): Json<VerifyTokenRequest>
+) -> impl IntoResponse {
+    
+    // validate token from auth.rs
+    match validate_token(&request.token).await {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(_) => Err(AuthAPIError::InvalidToken)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct VerifyTokenRequest {
+    token: String,
 }
